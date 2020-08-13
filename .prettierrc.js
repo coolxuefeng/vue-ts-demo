3f6ab96adb1d94ce7547e73444ff8a0f16c974b4
coolxuefeng <coolxuefeng@qq.com>
2020-08-13 15:32:28 +0800

    code generated by vue-cli

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  requirePragma: false,
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  eslintIntegration: true,
+};
